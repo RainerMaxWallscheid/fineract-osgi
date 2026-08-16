8724768711a2299ed140cbc5048b97254f149421
RainerMaxWallscheid <rainer.wallscheid@web.de>
2026-08-16 10:52:38 +0200

    Close leftover event and mapped module test residual.
    Move leftover tests next to already-moved production: event producers/serializers to event-test, core event/instance-mode tests to core, enricher/ExternalEventService to investor-test, plus leftover businessdate, notification, bulkimport, and campaigns tests.

--- fineract-bulkimport/test/src/test/java/org/apache/fineract/infrastructure/bulkimport/impl/osgi/BulkImportOsgiServiceRegistrarTest.java
+++ fineract-bulkimport/test/src/test/java/org/apache/fineract/infrastructure/bulkimport/impl/osgi/BulkImportOsgiServiceRegistrarTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.apache.fineract.infrastructure.bulkimport.service.BulkImportWorkbookPopulatorService;
 import org.apache.fineract.infrastructure.bulkimport.service.BulkImportWorkbookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ObjectProvider;
@@ -14,8 +15,10 @@ class BulkImportOsgiServiceRegistrarTest {
     @SuppressWarnings("unchecked")
     void afterPropertiesSet_withoutOsgi_doesNotThrow() {
         ObjectProvider<BulkImportWorkbookService> w = mock(ObjectProvider.class);
+        ObjectProvider<BulkImportWorkbookPopulatorService> p = mock(ObjectProvider.class);
         when(w.getIfAvailable()).thenReturn(null);
-        var reg = new BulkImportOsgiServiceRegistrar(w);
+        when(p.getIfAvailable()).thenReturn(null);
+        var reg = new BulkImportOsgiServiceRegistrar(w, p);
         assertDoesNotThrow(reg::afterPropertiesSet);
         assertDoesNotThrow(reg::destroy);
     }
